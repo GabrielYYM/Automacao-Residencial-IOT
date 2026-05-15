028903ed1bb4897cc5ff1cc67e6205c6a815d9cf
GabrielYYM <gabrielyyminato@gmail.com>
2026-05-15 20:59:11 -0300

    Add: initial project files for MicroPython simulation with Wokwi

---
+++ main.py
@@ -0,0 +1,15 @@
+from machine import Pin, ADC
+import dht
+
+class Sensor: 
+    # pir-motion-sensor = 23
+    # dht22 = 22
+    # gas-sensor = 34 Aout, 21 Dout
+    # photoresistor-sensor = 35 AO, 19 DO
+
+class Relays:
+    # Relay = 18
+    # Relay = 17
+    # Relay = 16
+    # Relay = 4
+
